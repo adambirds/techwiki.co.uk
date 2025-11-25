64faae4442f7db9c00f6ebe3ad493c68ddc422b2
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2025-11-25 23:36:25 +0000

    website: Add favicons.

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -14,6 +14,12 @@ export default async function RootLayout({
     return (
         <html lang="en" className="h-full" suppressHydrationWarning>
             <head>
+                <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <meta name="apple-mobile-web-app-title" content="Wedding of Rebecca & Peter" />
+                <link rel="manifest" href="/site.webmanifest" />
                 <meta
                     httpEquiv="Permissions-Policy"
                     content="picture-in-picture '*'"
